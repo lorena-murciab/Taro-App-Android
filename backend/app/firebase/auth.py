54357e25040ab242ac45238bc3b286a9e3eb217a
Lorena Murcia Belmonte <98983060+lorena-murciab@users.noreply.github.com>
2025-02-24 20:37:42 +0100

    Manteniendo solo frontend en la rama frontend

--- backend/app/firebase/auth.py
+++
@@ -1,9 +0,0 @@
-from firebase_admin import auth
-
-def verify_token(id_token):
-    """Verifica el token de Firebase Authentication."""
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        return decoded_token
-    except Exception:
-        return None
